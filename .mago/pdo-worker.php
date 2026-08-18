041ac134bd60efd5a67d3e105d369c4e897d4f2d
Jiri Velek <jiri.velek@yahoo.com>
2026-08-18 19:39:11 +0200

    fix(worker): dirname is now correct

--- .mago/pdo-worker.php
+++ .mago/pdo-worker.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Bleksak\MagoPdoExtension\Mago\PdoExtension;
 use Mago\Sdk\Worker;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__, 4) . '/vendor/autoload.php';
 
 new Worker(PdoExtension::create())->run();
